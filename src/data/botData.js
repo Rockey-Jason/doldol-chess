be3d4ceaa6e476b33f85671881908a1461e30a62
Rockey-Jason <jason.yeonwoo.im@gmail.com>
2026-08-03 18:47:58 +0900

    fix assets path

--- src/data/botData.js
+++ src/data/botData.js
@@ -3,49 +3,49 @@ const botData = {
     talc:{
         name:"Talc Rockey",
         level:1,
-        image:"/bots/talc.jpg",
+        image:"${import.meta.env.BASE_URL}bots/talc.jpg",
         ratingReward:10
     },
 
     sleep:{
         name:"Sleeping Rockey",
         level:2,
-        image:"/bots/sleeping.jpg",
+        image:"${import.meta.env.BASE_URL}bots/sleeping.jpg",
         ratingReward:30
     },
 
     fur:{
         name:"Rockey's Fur",
         level:4,
-        image:"/bots/fur.jpg",
+        image:"${import.meta.env.BASE_URL}bots/fur.jpg",
         ratingReward:140
     },
 
     rockey:{
         name:"Rockey",
         level:6,
-        image:"/bots/rockey.jpg",
+        image:"${import.meta.env.BASE_URL}bots/rockey.jpg",
         ratingReward:300
     },
 
     army:{
         name:"Rockey Army",
         level:8,
-        image:"/bots/army.jpg",
+        image:"${import.meta.env.BASE_URL}bots/army.jpg",
         ratingReward:1000
     },
 
     doronum:{
         name:"Doronum",
         level:10,
-        image:"/bots/doronum.jpg",
+        image:"${import.meta.env.BASE_URL}bots/doronum.jpg",
         ratingReward:2000
     },
 
     brilliant:{
         name:"Brilliant Rockey",
         level:20,
-        image:"/bots/brilliant.jpg",
+        image:"${import.meta.env.BASE_URL}bots/brilliant.jpg",
         ratingReward:3000
     }
 
